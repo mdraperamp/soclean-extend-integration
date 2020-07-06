241608080259a65417e56b5289cc23bea1db176a
mdraperng <mdraper@netgain.tech>
2020-07-06 09:00:29 -0500

    Adding production credentials

--- src/FileCabinet/SuiteScripts/ExtendWarrantyModule/Libs/customscript_amp_lib_keys.js
+++ src/FileCabinet/SuiteScripts/ExtendWarrantyModule/Libs/customscript_amp_lib_keys.js
@@ -13,9 +13,9 @@ define([
         email: 'mdraper@aimpointtech.com'
     };
     const objProd = {
-        storeId: '',
-        key : '',
-        domain: ''
+        storeId: '1db3230b-662c-4aee-a296-c7b07ff2d354',
+        key : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvZStzb2NsZWFuQGV4dGVuZC5jb20iLCJhY2NvdW50SWQiOiI3OTA4ZWNkNi0zNWYzLTRhNTgtYWVlZC1hMDJmZTQxMTkzOTQiLCJhY3RpdmF0ZWRBdCI6MTU5MzcxNzY4NzMxNywic2NvcGUiOiJhbGwiLCJpYXQiOjE1OTM3MTc3NTQsImV4cCI6MjU0OTk2MDE4NTUzLCJpc3MiOiJhcGkuaGVsbG9leHRlbmQuY29tIiwic3ViIjoiMzlkNzFiNjEtN2M2Zi00MjZlLTkzMWYtYWJmMmNiYmQwNjRiIiwianRpIjoiZjc2ODBlZmUtNWEzZi00MzJiLWFhODktYmJiNjIzN2VmMTI5In0.cNuFWIpLIy4mVkkmTahHLVHJwsOo5pB19Kmz2ZLqInE',
+        domain: 'https://api.helloextend.com'
     };
 
     var objKeys = {};
